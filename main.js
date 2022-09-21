05e7468c9bbb5f11665c9012a4e33f137ed0e79f
cristian maizon <ismael.maizon1234@gmail.com>
2022-09-21 19:35:01 -0300

    agregando login

--- main.js
+++ main.js
@@ -114,70 +114,69 @@ for (const alfombra of alfom) {
 
 
 
-const search = document.querySelector("#search");
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+
+
+// si una etiqueta "form" tiene un boton, ala hacer click en el mismo
+// nos va a recargar la pagina. Lo que pasa es que el navegador
+// interpreta que al hacer clik estamos enviando algun tipo de informacion a algun lado.
 
 
 
-function filtrarPrecios(arr, filtro) {
-    const filtrado = arr.filter((el)=>{
-        return el.material.includes(filtro);
-    });
-    return filtrado;
+
+
+// constructor de obj
+const usuarios = [];
+
+function informacion(nombre, password) {
+    this.nombre = nombre;
+    this.password = password;
 }
 
 
-search.addEventListener("click", ()=>{
 
-    if (input1.value.toLowerCase() == "silla") {
-        console.log("sillas");
-        let resultado = filtrarPrecios(sill, input2.value.toLowerCase())
-        console.log(resultado);
-        console.log(input2.value.toLowerCase());
-        for (const el of resultado) {
-            let li = document.createElement('li');
-            li.innerHTML = `<div class = 'card'>
-            <p>Material: ${el.material}</p>
-            <p>Tipo: ${el.tipo}</p>
-            <p>Precio: $${el.precio}</p>
-            </div>`
-    
-            ul4.append(li);
-        }
-    } else if (input1.value.toLowerCase() == "puerta") {
-        console.log("puertas");
-        let resultado = filtrarPrecios(puert, input2.value.toLowerCase())
-        console.log(resultado);
-        console.log(input2.value.toLowerCase());
-        for (const el of resultado) {
-            let li = document.createElement('li');
-            li.innerHTML = `<div class = 'card'>
-            <p>Material: ${el.material}</p>
-            <p>Tipo: ${el.tipo}</p>
-            <p>Precio: $${el.precio}</p>
-            </div>`
-    
-            ul4.append(li);
-        }
-    } else {
+button1.addEventListener('click', (e) => {
+    e.preventDefault();
 
-        console.log("alfombras");
-        let resultado = filtrarPrecios(alfom, input2.value.toLowerCase())
-        console.log(resultado);
-        console.log(input2.value.toLowerCase());
-        for (const el of resultado) {
-            let li = document.createElement('li');
-            li.innerHTML = `<div class = 'card'>
-            <p>Material: ${el.material}</p>
-            <p>Tipo: ${el.tipo}</p>
-            <p>Precio: $${el.precio}</p>
-            </div>`
-    
-            ul4.append(li);
-        }
-    }
-
+    const nuevoUsuario = new informacion(username.value, password.value);
+    usuarios.push(nuevoUsuario);
+                    
+    console.log(usuarios);
+    localStorage.setItem('informacion', JSON.stringify(usuarios));
 
 });
 
 
+button2.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const info = JSON.parse(localStorage.getItem('informacion'))
+
+    console.log(info);
+    
+    for (const element of info) {
+
+        const info2 = new informacion(element.nombre, element.password)
+
+        usuarios.push(info2)
+
+    }
+
+    for (const el of usuarios) {
+        
+        console.log('nombre: ' + el.nombre);
+        console.log('clave: ' + el.password);
+
+
+        if (username.value === el.nombre & password.value === el.password) {
+            window.location = "../index.html"
+        } else {
+            console.log('no ingresó');
+        }
+    }
+
+});
 
